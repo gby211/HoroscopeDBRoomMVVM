b8a4d298118a828ba5b24bbca29821150d8edb6c
TTawtet <gby211@yandex.ru>
2021-09-19 02:19:17 +0300

    add очень много всего

--- app/src/main/java/com/example/horoscopedbroommvvm/Presentation/Model/HoroscopeDTO.java
+++ app/src/main/java/com/example/horoscopedbroommvvm/Presentation/Model/HoroscopeDTO.java
@@ -16,4 +16,35 @@ public class HoroscopeDTO {
 
     public String info;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getZodiac() {
+        return zodiac;
+    }
+
+    public void setZodiac(String zodiac) {
+        this.zodiac = zodiac;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
 }
---
+++ app/src/main/java/com/example/horoscopedbroommvvm/Presentation/View/Adapter/HoroscopeRVAdapter.java
@@ -0,0 +1,61 @@
+package com.example.horoscopedbroommvvm.Presentation.View.Adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.horoscopedbroommvvm.Presentation.Model.HoroscopeDTO;
+import com.example.horoscopedbroommvvm.R;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HoroscopeRVAdapter extends RecyclerView.Adapter<HoroscopeRVAdapter.HoroscopeViewHolder> {
+
+    private List<HoroscopeDTO> horoscopeDTOS = new ArrayList<>();
+
+    @NonNull
+    @NotNull
+    @Override
+    public HoroscopeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.recyclerview_item,parent,false);
+        return new HoroscopeViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull HoroscopeViewHolder holder, int position) {
+        HoroscopeDTO DTO = horoscopeDTOS.get(position);
+        holder.textViewDate.setText(DTO.getDate());
+        holder.textViewZodiac.setText(DTO.getZodiac());
+        holder.textViewInfo.setText(DTO.getInfo());
+    }
+
+    @Override
+    public int getItemCount() {
+        return horoscopeDTOS.size();
+    }
+
+    public List<HoroscopeDTO> getData() {
+        return horoscopeDTOS;
+    }
+
+    public class HoroscopeViewHolder extends RecyclerView.ViewHolder {
+        private TextView textViewZodiac;
+        private TextView textViewDate;
+        private TextView textViewInfo;
+
+
+        public HoroscopeViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textViewZodiac = itemView.findViewById(R.id.textViewZodiac);
+            textViewDate = itemView.findViewById(R.id.textViewDate);
+            textViewInfo = itemView.findViewById(R.id.textViewInfo);
+        }
+    }
+}
