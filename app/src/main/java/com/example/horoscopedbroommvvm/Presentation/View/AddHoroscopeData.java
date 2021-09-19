ba05319c09f03c65a465ba338253dce36d7023e5
TTawtet <gby211@yandex.ru>
2021-09-19 03:03:39 +0300

    add

--- app/src/main/java/com/example/horoscopedbroommvvm/Presentation/View/AddHoroscopeData.java
+++ app/src/main/java/com/example/horoscopedbroommvvm/Presentation/View/AddHoroscopeData.java
@@ -23,7 +23,7 @@ public class AddHoroscopeData extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_add_horoscope_data, container, false);
     }
 }
